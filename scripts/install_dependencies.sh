b488f9dbb05c946af6c730392ecc7ee1e1a6145c
waleed <waleedbakar@outlook.com>
2023-11-15 15:53:27 +0000

    install

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,1 +1,12 @@
 #!/bin/bash 
+
+cd /var/acebook
+curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+source ~/.bashrc
+nvm install 18
+nvm use 18
+npm install
+npm install -g pm2
