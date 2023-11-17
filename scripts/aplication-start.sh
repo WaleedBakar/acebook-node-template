c2a3d5a2348ae87a57450507a119cf77cfa93ef8
waleed <waleedbakar@outlook.com>
2023-11-17 09:51:23 +0000

    :(

---
+++ scripts/aplication-start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Navigate to your app directory
+cd /var/acebook
+# Start your application with pm2
+pm2 start app.js --name waleed-acebook
---
+++ scripts/application-stop.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cd /var/acebook
+source /home/ec2-user/.bash_profile
+APP_NAME="waleed-acebook2"
+# Check if the application is running
+if pm2 describe $APP_NAME &> /dev/null; then
+    # If running, stop and delete the application
+    pm2 stop $APP_NAME
+    pm2 delete $APP_NAME
+    echo "Application $APP_NAME stopped."
+else
+    echo "Application $APP_NAME is not running."
+fi
